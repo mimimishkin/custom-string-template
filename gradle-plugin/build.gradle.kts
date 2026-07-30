ddcc97bf18ad834f990f531361bed021b223b365
mika <printf.mika@gmail.com>
2026-07-30 17:15:14 +0300

    Bring publishing setting back

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -45,3 +45,33 @@ buildConfig {
     buildConfigField<String>("PLUGIN_VERSION", compilerPlugin.version!!)
     buildConfigField<String>("RUNTIME_LIBRARY", runtime.toString())
 }
+
+mavenPublishing {
+    coordinates(groupId = group.toString(), artifactId = "custom-string-template", version = version.toString())
+
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    pom {
+        name = "Custom StringTemplate gradle plugin"
+        description = project.description
+        inceptionYear = "2026"
+        url = "https://github.com/mimimishkin/custom-string-template"
+        licenses {
+            license {
+                name = "MIT"
+            }
+        }
+        developers {
+            developer {
+                id = "mimimishkin"
+                name = "Mimimishkin"
+                email = "printf.mika@gmail.com"
+            }
+        }
+        scm {
+            url = "https://github.com/mimimishkin/custom-string-template"
+            connection = "scm:git:git://github.com/mimimishkin/custom-string-template"
+        }
+    }
+}
